bfae524e1991e3cfb609ebacfcf2d329f0ddb9ed
kashiash <kashiash@gmail.com>
2022-07-04 23:45:31 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jacek Placek on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
